1cb56a018ac1920a91af171b62fb1f5c1b6aabd9
Adrian Tarau <adrian.tarau@gmail.com>
2024-06-25 08:48:24 -0400

    Move libraries to Bootstrap-JS project

--- js/src/main/resources/js/adminator/adminator.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $(".sidebar .dropdown-toggle").on("click", function(event){
-        let dropdownParent=$(this.parentElement);
-        let dropdownMenu=dropdownParent.find('.dropdown-menu');
-        dropdownMenu.toggle();
-    });
-});
--- js/src/main/resources/js/misc/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function load() {
-    const loader = $('loader');
-    setTimeout(function () {
-        loader.addClass('fadeOut');
-    }, 300);
-});
