673c7cc4b033ef70b55e422f39ce4cb7fc349458
Adrian Tarau <adrian.tarau@gmail.com>
2024-08-04 12:04:58 -0400

    Remove @EnableJpaRepositories & @EntityScan from Bootstrap, they are processed once. The application needs to enable them.

--- jdbc/src/main/java/net/microfalx/bootstrap/jdbc/config/JpaConfig.java
+++ jdbc/src/main/java/net/microfalx/bootstrap/jdbc/config/JpaConfig.java
@@ -1,13 +1,7 @@
 package net.microfalx.bootstrap.jdbc.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration("bootstrap-jpa")
-@EnableJpaRepositories("net.microfalx.bootstrap")
-@EntityScan("net.microfalx.bootstrap")
-@EnableTransactionManagement
+@Configuration
 public class JpaConfig {
 }
