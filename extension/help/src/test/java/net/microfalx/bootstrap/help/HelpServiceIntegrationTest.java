8a0b804c72bba6eddb5f47d45bb5af9b9b14a116
Adrian Tarau <adrian.tarau@gmail.com>
2025-08-23 10:57:54 -0400

    Remove dependency-cycle

--- extension/help/src/test/java/net/microfalx/bootstrap/help/HelpServiceIntegrationTest.java
+++ extension/help/src/test/java/net/microfalx/bootstrap/help/HelpServiceIntegrationTest.java
@@ -1,13 +1,23 @@
 package net.microfalx.bootstrap.help;
 
 import net.microfalx.bootstrap.content.ContentService;
+import net.microfalx.bootstrap.core.async.AsynchronousConfig;
+import net.microfalx.bootstrap.core.i18n.I18nProperties;
+import net.microfalx.bootstrap.core.i18n.I18nService;
+import net.microfalx.bootstrap.model.MetadataService;
+import net.microfalx.bootstrap.resource.ResourceProperties;
+import net.microfalx.bootstrap.resource.ResourceService;
 import net.microfalx.bootstrap.search.IndexService;
 import net.microfalx.bootstrap.search.SearchService;
-import net.microfalx.bootstrap.test.AbstractBootstrapServiceTestCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.validation.beanvalidation.OptionalValidatorFactoryBean;
 
 import java.time.Duration;
 import java.util.List;
@@ -15,9 +25,13 @@ import java.util.List;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ContextConfiguration(classes = {ContentService.class, IndexService.class, SearchService.class,
-        HelpService.class})
-public class HelpServiceIntegrationTest extends AbstractBootstrapServiceTestCase {
+@ContextConfiguration(classes = {I18nService.class, ResourceService.class, ResourceProperties.class, OptionalValidatorFactoryBean.class,
+        MetadataService.class, ContentService.class, IndexService.class, SearchService.class, HelpService.class})
+@Import({I18nProperties.class, AsynchronousConfig.class})
+@OverrideAutoConfiguration(enabled = false)
+@ImportAutoConfiguration
+@SpringBootTest
+public class HelpServiceIntegrationTest {
 
     @Autowired
     private HelpService helpService;
