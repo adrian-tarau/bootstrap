8cb200581758cacc10161e57858051daed30862a
Adrian Tarau <adrian.tarau@gmail.com>
2025-12-29 10:01:33 -0500

    Add bean name for AuditRepository

--- extension/rest-api-client/src/main/java/net/microfalx/bootstrap/restapi/client/jpa/AuditRepository.java
+++ extension/rest-api-client/src/main/java/net/microfalx/bootstrap/restapi/client/jpa/AuditRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("RestApiAuditRepository")
 public interface AuditRepository extends JpaRepository<Audit, Integer>, JpaSpecificationExecutor<Audit> {
 }
