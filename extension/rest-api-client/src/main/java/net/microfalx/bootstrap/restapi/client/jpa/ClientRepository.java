f80f4ff4a3b1f50e33638a07380d1df0bbed8d7f
Adrian Tarau <adrian.tarau@gmail.com>
2025-12-29 10:05:53 -0500

    Add bean name for ClientRepository

--- extension/rest-api-client/src/main/java/net/microfalx/bootstrap/restapi/client/jpa/ClientRepository.java
+++ extension/rest-api-client/src/main/java/net/microfalx/bootstrap/restapi/client/jpa/ClientRepository.java
@@ -4,6 +4,6 @@ import net.microfalx.bootstrap.jdbc.jpa.NaturalJpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("RestApiClientRepository")
 public interface ClientRepository extends NaturalJpaRepository<Client, Integer>, JpaSpecificationExecutor<Client> {
 }
