3ac1e3088c1c6ef5d42b1d78b680e450a0564ed9
Adrian Tarau <adrian.tarau@gmail.com>
2026-01-05 18:40:13 -0500

    Add logging (once) how templates are resolved and if they exist

--- extension/web/src/main/java/net/microfalx/bootstrap/web/template/TemplateConfiguration.java
+++ extension/web/src/main/java/net/microfalx/bootstrap/web/template/TemplateConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
@@ -21,12 +23,14 @@ public class TemplateConfiguration {
     private ApplicationContext applicationContext;
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public ITemplateResolver getViewResolver() {
         ThemeAwareTemplateResolver resolver = new ThemeAwareTemplateResolver(applicationService);
         resolver.setApplicationContext(this.applicationContext);
         resolver.setPrefix(this.properties.getPrefix());
         resolver.setSuffix(this.properties.getSuffix());
         resolver.setTemplateMode(this.properties.getMode());
+        resolver.setOrder(-1);
         if (this.properties.getEncoding() != null) {
             resolver.setCharacterEncoding(this.properties.getEncoding().name());
         }
--- extension/web/src/main/java/net/microfalx/bootstrap/web/template/ThemeAwareTemplateResolver.java
+++ extension/web/src/main/java/net/microfalx/bootstrap/web/template/ThemeAwareTemplateResolver.java
@@ -1,7 +1,9 @@
 package net.microfalx.bootstrap.web.template;
 
+import lombok.extern.slf4j.Slf4j;
 import net.microfalx.bootstrap.web.application.ApplicationService;
 import net.microfalx.bootstrap.web.application.Theme;
+import org.slf4j.event.Level;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
@@ -9,17 +11,21 @@ import org.thymeleaf.IEngineConfiguration;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * A view resolver which is aware of the application template.
  */
+@Slf4j
 public class ThemeAwareTemplateResolver extends SpringResourceTemplateResolver {
 
-    private ApplicationService applicationService;
+    private final ApplicationService applicationService;
     private ApplicationContext applicationContext;
 
-    private Map<String, Boolean> themedResources = new ConcurrentHashMap<>();
+    private final Map<String, Boolean> themedResources = new ConcurrentHashMap<>();
+    private final Set<String> resolvedTemplates = new CopyOnWriteArraySet<>();
 
     public ThemeAwareTemplateResolver(ApplicationService applicationService) {
         this.applicationService = applicationService;
@@ -47,7 +53,19 @@ public class ThemeAwareTemplateResolver extends SpringResourceTemplateResolver {
             themedResources.put(template, hasThemedTemplate);
         }
         if (hasThemedTemplate) suffix = suffixWithTheme;
-        return super.computeResourceName(configuration, ownerTemplate, template, prefix, suffix, forceSuffix,
+        String resourceUri = super.computeResourceName(configuration, ownerTemplate, template, prefix, suffix, forceSuffix,
                 templateAliases, templateResolutionAttributes);
+        if (resolvedTemplates.add(resourceUri)) {
+            boolean exists = applicationContext.getResource(resourceUri).exists();
+            org.slf4j.event.Level level = exists ? Level.INFO : Level.ERROR;
+            if (ownerTemplate != null) {
+                LOGGER.atLevel(level).log("Resolve template '{}' (owner template '{}') with themed resource URI '{}', themed {}, exists {}",
+                        template, ownerTemplate, resourceUri, hasThemedTemplate, exists);
+            } else {
+                LOGGER.atLevel(level).log("Resolve template '{}' with themed resource URI '{}', themed {}, exists {}",
+                        template, resourceUri, hasThemedTemplate, exists);
+            }
+        }
+        return resourceUri;
     }
 }
