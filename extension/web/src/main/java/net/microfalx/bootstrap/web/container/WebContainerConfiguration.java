ae11a35d97dc64f377867aff1cc2fe3f2e220242
Adrian Tarau <adrian.tarau@gmail.com>
2026-01-11 13:35:18 -0500

    Enable ForwardedHeaderFilter with the right order

--- extension/web/src/main/java/net/microfalx/bootstrap/web/container/WebContainerConfiguration.java
+++ extension/web/src/main/java/net/microfalx/bootstrap/web/container/WebContainerConfiguration.java
@@ -1,14 +1,19 @@
 package net.microfalx.bootstrap.web.container;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class WebContainerConfiguration {
 
     @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean =
+                new FilterRegistrationBean<>(new ForwardedHeaderFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
