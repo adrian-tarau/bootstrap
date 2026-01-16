4177ece510544f4269fa61b65e927cf7837ed57d
Adrian Tarau <adrian.tarau@gmail.com>
2026-01-16 09:10:25 -0500

    Autoselect the theme (dark/light) based on OS preferences

--- extension/web/src/main/resources/js/app/core.js
+++ extension/web/src/main/resources/js/app/core.js
@@ -744,6 +744,22 @@ Application.initTimeZone = function () {
 }
 
 /**
+ * Initialize the theme.
+ */
+Application.initTheme = function () {
+    function updateTheme() {
+        const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        document.documentElement.setAttribute("data-bs-theme", colorMode);
+    }
+
+    // Set theme on load
+    updateTheme();
+
+    // Update theme when the preferred scheme changes
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
+}
+
+/**
  * Initializes the application
  */
 Application.initialize = function () {
