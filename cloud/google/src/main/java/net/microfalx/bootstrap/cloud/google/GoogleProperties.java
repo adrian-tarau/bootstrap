90864a8a5fac8aa6176f83cc9509e4a486d20cf7
Adrian Tarau <adrian.tarau@gmail.com>
2026-02-04 15:51:34 -0500

    Add Google Address API key setting

--- cloud/google/src/main/java/net/microfalx/bootstrap/cloud/google/GoogleProperties.java
+++ cloud/google/src/main/java/net/microfalx/bootstrap/cloud/google/GoogleProperties.java
@@ -6,12 +6,19 @@ import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import static net.microfalx.lang.StringUtils.isEmpty;
+
 @Configuration
 @ConfigurationProperties("bootstrap.cloud.google")
 @Getter
 @Setter
 @ToString
 public class GoogleProperties {
+
+    /**
+     * API key for Google Address services. If not provided, Google Address services will be disabled.
+     */
+    private String addressApiKey;
 
     /**
      * API key for Google Maps services. If not provided, Google Maps services will be disabled.
@@ -22,4 +29,40 @@ public class GoogleProperties {
      * Map identifier for Google Maps services.
      */
     private String mapId;
+
+    /**
+     * Returns the Google Address API key.
+     *
+     * @return a non-null instance
+     */
+    public String getAddressApiKey() {
+        if (isEmpty(addressApiKey)) {
+            throw new IllegalStateException("Google Address API key is not set");
+        }
+        return addressApiKey;
+    }
+
+    /**
+     * Returns the Google Maps API key.
+     *
+     * @return a non-null instance
+     */
+    public String getMapApiKey() {
+        if (isEmpty(mapApiKey)) {
+            throw new IllegalStateException("Google Address API key is not set");
+        }
+        return mapApiKey;
+    }
+
+    /**
+     * Returns the Google Maps Map ID.
+     *
+     * @return a non-null instance
+     */
+    public String getMapId() {
+        if (isEmpty(mapId)) {
+            throw new IllegalStateException("Google Address API key is not set");
+        }
+        return mapId;
+    }
 }
