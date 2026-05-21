8d4d0832769836861b4e90c44f18d223e7aff243
Adrian Tarau <adrian.tarau@gmail.com>
2026-05-21 16:42:52 -0400

    Add a nicer thread name prefix for Jetty
    (cherry picked from commit 1f91b694fc440008dcb12b6b5b9e425987fa50e0)

--- extension/web/src/main/java/net/microfalx/bootstrap/web/container/WebContainerConfiguration.java
+++ extension/web/src/main/java/net/microfalx/bootstrap/web/container/WebContainerConfiguration.java
@@ -1,5 +1,8 @@
 package net.microfalx.bootstrap.web.container;
 
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +18,15 @@ public class WebContainerConfiguration {
                 new FilterRegistrationBean<>(new ForwardedHeaderFilter());
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<JettyServletWebServerFactory> jettyThreadPoolCustomizer() {
+        return factory -> factory.addServerCustomizers(server -> {
+            QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
+            if (threadPool != null) {
+                threadPool.setName("Boot-Web");
+            }
+        });
     }
 }
