c4f766408080f78bb6dbbd8d79d2d43041dcdbca
Adrian Tarau <adrian.tarau@gmail.com>
2026-05-23 21:50:47 -0400

    Show process libraries under Support

---
+++ extension/support/src/main/java/net/microfalx/bootstrap/support/misc/Library.java
@@ -0,0 +1,95 @@
+package net.microfalx.bootstrap.support.misc;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import net.microfalx.bootstrap.dataset.Alert;
+import net.microfalx.bootstrap.dataset.annotation.Filterable;
+import net.microfalx.bootstrap.dataset.annotation.Formattable;
+import net.microfalx.bootstrap.dataset.model.Formatters;
+import net.microfalx.bootstrap.dataset.model.NamedIdentityAware;
+import net.microfalx.bootstrap.model.Field;
+import net.microfalx.lang.annotation.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Name("Libraries")
+@ReadOnly
+@Visible(value = false, fieldNames = "description")
+public class Library extends NamedIdentityAware<String> {
+
+    @Position(20)
+    @Label(value = "Vendor", group = "Implementation")
+    @Description("The vendor which provides the library or specification (API)")
+    @Width("200px")
+    private String vendor;
+
+    @Position(21)
+    @Label(value = "Version", group = "Implementation")
+    @Description("The version of the library implementation or specification (API)")
+    @Width("80px")
+    @Filterable
+    private String version;
+
+    @Position(22)
+    @Label(value = "Build", group = "Implementation")
+    @Description("The build number/commit hash of the library implementation or specification (API)")
+    @Width("80px")
+    @Filterable
+    private String build;
+
+    @Position(22)
+    @Label(value = "Number", group = "Build")
+    @Description("The build number (commit hash) of the library")
+    @Width("80px")
+    @Filterable
+    private String buildNumber;
+
+    @Position(30)
+    @Label(value = "Time", group = "Build")
+    @Description("The build time of the library")
+    @Width("80px")
+    @Filterable
+    private String buildTime;
+
+    @Position(60)
+    @Label(value = "Name", group = "File")
+    @Description("The file name of the library")
+    @Width("180px")
+    @Filterable
+    private String fileName;
+
+    @Position(61)
+    @Description("The size of the file")
+    @Label(value = "Size", group = "File")
+    @Formattable(unit = Formattable.Unit.BYTES)
+    @Width("60px")
+    private long fileSize;
+
+    @Position(62)
+    @Label(value = "Modified At", group = "File")
+    @Description("The modification time of the library")
+    @Formattable(tooltip = Formatters.LastAccessedTooltip.class, elapsed = true)
+    @Width("120px")
+    private LocalDateTime fileLastModified;
+
+    @Position(63)
+    @Label(value = "Info")
+    @Description("The additional attributes/information available for the library")
+    @Width("40px")
+    @Formattable(alert = InfoProvider.class)
+    private String info;
+
+    public static class InfoProvider implements Formattable.AlertProvider<Library, Field<Library>, String> {
+
+        @Override
+        public Alert provide(String value, Field<Library> field, Library model) {
+            Alert.Type type = Alert.Type.INFO;
+            return Alert.builder().type(type).icon(Alert.Icon.INFORMATION)
+                    .message(model.getDescription()).build();
+        }
+    }
+}
---
+++ extension/support/src/main/java/net/microfalx/bootstrap/support/misc/LibraryDataSet.java
@@ -0,0 +1,48 @@
+package net.microfalx.bootstrap.support.misc;
+
+import net.microfalx.bootstrap.dataset.DataSetFactory;
+import net.microfalx.bootstrap.dataset.PojoDataSet;
+import net.microfalx.bootstrap.model.Filter;
+import net.microfalx.bootstrap.model.Metadata;
+import net.microfalx.bootstrap.model.PojoField;
+import net.microfalx.lang.JvmUtils;
+import net.microfalx.lang.annotation.Provider;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static net.microfalx.lang.StringUtils.defaultIfEmpty;
+import static net.microfalx.lang.TimeUtils.toLocalDateTime;
+
+@Provider
+public class LibraryDataSet extends PojoDataSet<Library, PojoField<Library>, String> {
+
+    public LibraryDataSet(DataSetFactory<Library, PojoField<Library>, String> factory, Metadata<Library, PojoField<Library>, String> metadata) {
+        super(factory, metadata);
+    }
+
+    @Override
+    protected Page<Library> doFindAll(Pageable pageable, Filter filterable) {
+        List<Library> stores = JvmUtils.getJars().stream().map(this::from).collect(Collectors.toList());
+        return getPage(stores, pageable, filterable);
+    }
+
+    private Library from(JvmUtils.Jar jar) {
+        Library library = new Library();
+        library.setId(jar.getId());
+        library.setName(jar.getName());
+        library.setVendor(defaultIfEmpty(jar.getImplementationVendor(), jar.getSpecificationVendor()));
+        library.setVersion(defaultIfEmpty(jar.getImplementationVersion().toString(), jar.getSpecificationVersion()));
+        library.setBuild(jar.getImplementationBuild());
+        library.setBuildNumber(jar.getImplementationBuild());
+        library.setBuildTime(jar.getBuildTime());
+        library.setFileName(jar.getFile().getName());
+        library.setFileSize(jar.getFile().length());
+        library.setDescription(jar.getDescription());
+        library.setFileLastModified(toLocalDateTime(jar.getFile().lastModified()));
+        library.setInfo(Integer.toString(jar.getAttributeCount()));
+        return library;
+    }
+}
