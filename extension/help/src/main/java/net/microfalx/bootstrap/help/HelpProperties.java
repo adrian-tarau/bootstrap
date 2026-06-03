cda557feed2df33b59a6e8585315dc4fcdcc3b96
Adrian Tarau <adrian.tarau@gmail.com>
2026-06-03 15:04:57 -0400

    Allow the help to provide which theme and mode to use
    (cherry picked from commit c70400e4d7d716a39a0d34931feaa24eeb878fff)

--- extension/help/src/main/java/net/microfalx/bootstrap/help/HelpProperties.java
+++ extension/help/src/main/java/net/microfalx/bootstrap/help/HelpProperties.java
@@ -17,4 +17,17 @@ public class HelpProperties {
      * Indicates whether the help should be accessed only if a security context exits.
      */
     private boolean secure = true;
+
+    /**
+     * Indicates whether the help should be rendered with a specific theme.
+     * <p>
+     */
+    private String theme;
+
+    /**
+     * Indicates which theme mode  should be used for the help.
+     * <p>
+     * If not provided, it will pick the mode of the current theme.
+     */
+    private String themeMode;
 }
