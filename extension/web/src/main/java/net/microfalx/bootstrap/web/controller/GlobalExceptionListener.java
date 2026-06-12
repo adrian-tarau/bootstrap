8bc77ba856e66c5c5b3f6ef7755fc9fd0ab849f9
Adrian Tarau <adrian.tarau@gmail.com>
2026-06-12 11:42:36 -0400

    Do not track invalid PHP requests as issues

--- extension/web/src/main/java/net/microfalx/bootstrap/web/controller/GlobalExceptionListener.java
+++ extension/web/src/main/java/net/microfalx/bootstrap/web/controller/GlobalExceptionListener.java
@@ -5,37 +5,70 @@ import jakarta.servlet.http.HttpServletResponse;
 import net.microfalx.bootstrap.core.utils.Failure;
 import net.microfalx.bootstrap.support.report.Issue;
 import net.microfalx.bootstrap.web.util.PathFilter;
+import net.microfalx.lang.ClassUtils;
 import net.microfalx.lang.UriUtils;
+import net.microfalx.metrics.Metrics;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionListener implements HandlerExceptionResolver {
 
     private static final Map<Failure.Type, Issue.Severity> FAILURE_SEVERITIES = new HashMap<>();
+    private static final Set<String> FAILURE_PATH_IGNORES = new HashSet<>();
+    private static final PathFilter DEFAULT_EXCLUSIONS = new PathFilter();
+
+    private static final Metrics FAILURE = Metrics.of("Bootstrap").withGroup("Controller Failures");
+    private static final Metrics FAILURE_BY_EXCEPTION = FAILURE.withGroup("Exception");
+    private static final Metrics FAILURE_BY_PATH = FAILURE.withGroup("Path");
 
     @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
+        FAILURE_BY_EXCEPTION.count(ClassUtils.getCompactName(exception));
+        FAILURE_BY_PATH.count(PathFilter.getRootPath(request));
+        if (shouldIgnorePath(request) || shouldIgnoreException(request, exception)) return null;
         String matchedPattern = PathFilter.getRequestPattern(request);
-        Issue.Severity severity = getSeverityFromException(request, ex);
+        Issue.Severity severity = getSeverityFromException(request, exception);
         if (severity != null) {
-            Issue.create(Issue.Type.STABILITY, matchedPattern).withDescription(ex, "Unhandled exception in controller")
+            Issue.create(Issue.Type.STABILITY, matchedPattern).withDescription(exception, "Unhandled exception in controller")
                     .withModule("Controller").withSeverity(severity).withAttributeCounter(PathFilter.getRootPath(request))
                     .register();
         }
         return null;
     }
 
-    private Issue.Severity getSeverityFromException(HttpServletRequest request, Exception ex) {
+    private boolean shouldIgnoreException(HttpServletRequest request, Exception exception) {
+        Failure.Type type = Failure.getType(exception);
+        if (type == Failure.Type.AUTHORIZATION) {
+            return DEFAULT_EXCLUSIONS.isRoot(request) || DEFAULT_EXCLUSIONS.shouldExclude(request);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean shouldIgnorePath(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        PathMatcher matcher = new AntPathMatcher();
+        for (String pathIgnore : FAILURE_PATH_IGNORES) {
+            if (matcher.match(pathIgnore, uri)) return true;
+        }
+        return false;
+    }
+
+    private Issue.Severity getSeverityFromException(HttpServletRequest request, Exception exception) {
         String rootPath = PathFilter.getRootPath(request);
-        Failure.Type type = Failure.getType(ex);
+        Failure.Type type = Failure.getType(exception);
         if (type.isSecurity() && UriUtils.isRoot(rootPath)) return null;
         Issue.Severity severity = FAILURE_SEVERITIES.get(type);
         if (severity == null) severity = Issue.Severity.HIGH;
@@ -53,5 +86,7 @@ public class GlobalExceptionListener implements HandlerExceptionResolver {
         FAILURE_SEVERITIES.put(Failure.Type.RESOURCE_NOT_FOUND, Issue.Severity.LOW);
         FAILURE_SEVERITIES.put(Failure.Type.SERVICE_UNAVAILABLE, Issue.Severity.HIGH);
         FAILURE_SEVERITIES.put(Failure.Type.CONFIGURATION, Issue.Severity.CRITICAL);
+
+        FAILURE_PATH_IGNORES.add("/**/*.php");
     }
 }
