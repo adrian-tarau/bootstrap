69dc93eb80a4e9c9be688b25cd10e45c8a733aa7
Adrian Tarau <adrian.tarau@gmail.com>
2026-06-18 08:02:27 -0400

    Add more HTTP request exclusion patterns
    (cherry picked from commit bea8a443da6f8171dc137ec07c99b549d4bec6a2)

--- base/web/src/test/java/net/microfalx/bootstrap/web/util/PathFilterTest.java
+++ base/web/src/test/java/net/microfalx/bootstrap/web/util/PathFilterTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 
@@ -18,9 +19,11 @@ class PathFilterTest {
     private HttpServletRequest httpServletRequest;
     private String requestUri = "/";
 
+    private PathFilter filter = new PathFilter(true, true);
+
     @BeforeEach
     void setup() {
-        when(httpServletRequest.getRequestURI()).thenAnswer(invocation ->  requestUri);
+        when(httpServletRequest.getRequestURI()).thenAnswer(invocation -> requestUri);
     }
 
     @Test
@@ -34,6 +37,21 @@ class PathFilterTest {
         assertEquals("/a/b", PathFilter.getRootPath(httpServletRequest));
         requestUri = "/a/b/c/d";
         assertEquals("/a/b", PathFilter.getRootPath(httpServletRequest));
+    }
+
+    @Test
+    void isExcluded() {
+        assertExcluded("/dragonshell.php");
+        assertExcluded("/profile/wen.php");
+        assertExcluded("/win.asp");
+        assertExcluded("/.env.production");
+        assertExcluded("/services/.env");
+        assertExcluded("/.env.old");
+    }
+
+    private void assertExcluded(String path) {
+        requestUri = path;
+        assertTrue(filter.shouldExclude(httpServletRequest));
     }
 
 }
